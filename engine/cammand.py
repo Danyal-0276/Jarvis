26f10fc9939830099078f25c63cf01f0d20e57ff
Danyal Tanveer <donibutt2112@gmail.com>
2025-03-24 22:30:19 +0500

    Voice done Day 4

--- engine/cammand.py
+++ engine/cammand.py
@@ -1,4 +1,5 @@
 import pyttsx3
+import speech_recognition as sr
 
 def speak(text):
     engine=pyttsx3.init('sapi5')
@@ -9,5 +10,22 @@ def speak(text):
     engine.say(text)
     engine.runAndWait()
     
-    
-speak("Hello, I am your Assistant")    
+def takecammand():
+    r=sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold=1
+        r.adjust_for_ambient_noise
+        audio=r.listen(source,10,6)
+    try:
+        print("Recognizing...")
+        query=r.recognize_google(audio,language='en-in')
+        print(f"User said: {query}\n")
+    except Exception as e:
+        print("Say that again please...")
+        return "None"
+    return query.lower()
+        
+  
+text= takecammand()  
+speak(text)    
