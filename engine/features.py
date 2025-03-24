4f4de8759a9fdb9c60ca208b820b45a19f84e665
Danyal Tanveer <donibutt2112@gmail.com>
2025-03-24 21:38:25 +0500

    Day 4 Completed

--- engine/features.py
+++ engine/features.py
@@ -1,6 +1,8 @@
 from playsound import playsound
+import eel
 
 # Function to play the assistant sound when the assistant is started
+@eel.expose
 def playAssistantSound():
-    music_dir = "www/assets/audio/start_sound.mp3"
+    music_dir = "www/assets/audio/start_sound.wav"
     playsound(music_dir, True)
--- www/main.js
+++ www/main.js
@@ -1,29 +1,34 @@
 $(document).ready(function () {
-    $(".text").textillate({
-      loop: true,
-      sync: true,
-      in: { effect: "bounceIn" },
-      out: { effect: "bounceOut" },
-    });
-    var siriWave = new SiriWave({
-      container: document.getElementById("siri-container"),
-      width: 840,
-      height: 200,
-      speed: 0.30,
-      amplitude: 1,
-      frequency: 2,
-      style: "ios9",
-      autostart: true,
-    });
-
-    $(".siri-message").textillate({
-        loop: true,
-        sync: true,
-        in: { effect: "fadeInUp",
-            sync: true,
-         },
-        out: { effect: "fadeOutUp", 
-            sync: true,
-        },
-      });
+  $(".text").textillate({
+    loop: true,
+    sync: true,
+    in: { effect: "bounceIn" },
+    out: { effect: "bounceOut" },
   });
+  var siriWave = new SiriWave({
+    container: document.getElementById("siri-container"),
+    width: 840,
+    height: 200,
+    speed: 0.3,
+    amplitude: 1,
+    frequency: 2,
+    style: "ios9",
+    autostart: true,
+  });
+
+  $(".siri-message").textillate({
+    loop: true,
+    sync: true,
+    in: { effect: "fadeInUp", sync: true },
+    out: { effect: "fadeOutUp", sync: true },
+  });
+
+  // Mic Button click event
+  $("#MicBtn").click(function () {
+   
+    eel.playAssistantSound();
+    $("#Oval").attr("hidden", true);
+    $("#SiriWave").attr("hidden", false);
+  });
+
+});
